8b7b98bdceb420ec38a0acfd92373f0cc3077d99
Viktor Kuzhelny <viktor@marketto.ru>
2019-11-12 20:09:23 +0500

    v1.3.5

--- src/stubs/resources/js/router.js
+++ src/stubs/resources/js/router.js
@@ -15,35 +15,32 @@ export default new Router({
     {
       path: '/tables',
       name: 'tables',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "tables" */ './views/Tables.vue')
+      component: () => import('./views/Tables.vue')
     },
     {
       path: '/forms',
       name: 'forms',
-      component: () => import(/* webpackChunkName: "forms" */ './views/Forms.vue')
+      component: () => import('./views/Forms.vue')
     },
     {
       path: '/profile',
       name: 'profile',
-      component: () => import(/* webpackChunkName: "profile" */ './views/Profile.vue')
+      component: () => import('./views/Profile.vue')
     },
     {
       path: '/clients/index',
       name: 'clients.index',
-      component: () => import(/* webpackChunkName: "client" */ './views/Clients/ClientsIndex.vue'),
+      component: () => import('./views/Clients/ClientsIndex.vue'),
     },
     {
       path: '/clients/new',
       name: 'clients.new',
-      component: () => import(/* webpackChunkName: "client" */ './views/Clients/ClientsForm.vue'),
+      component: () => import('./views/Clients/ClientsForm.vue'),
     },
     {
       path: '/clients/:id',
       name: 'clients.edit',
-      component: () => import(/* webpackChunkName: "client" */ './views/Clients/ClientsForm.vue'),
+      component: () => import('./views/Clients/ClientsForm.vue'),
       props: true
     }
   ],
